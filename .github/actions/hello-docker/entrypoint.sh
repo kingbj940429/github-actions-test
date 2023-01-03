bc9c4ca08375dc10f5fcfd89bec6a43d77bc09a3
kingbj0429 <kingbj0429@lunit.io>
2023-01-03 11:58:42 +0900

    Create Action

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-if [ true ]
-then
-  echo "error"
-fi
-
-echo "::notice::Missing Class"
-echo "::debug::Debug Message"
-echo "::warning::Warning Message"
-echo "::error::Error Message"
-
-echo "::add-mask::Hello"
-
-echo "Hello Helo"
-
+echo "Hello $1"
 time=$(date)
-echo "::set-output name=time::$time"
+echo "time=$time" >> $GITHUB_OUTPUT
+echo "date=date1234" >> $GITHUB_OUTPUT
 
-echo "::group::Some expandable logs"
-echo "some stuff"
-echo "some stuff"
-echo "some stuff"
-echo "::endgroup::"
-
-
-echo $INPUT_TEST
-
-echo "action_state=yellow" >> $GITHUB_ENV
-env
+echo "${GITHUB_OUTPUT}"
